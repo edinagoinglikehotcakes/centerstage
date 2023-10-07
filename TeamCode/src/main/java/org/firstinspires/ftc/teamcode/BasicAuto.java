a73e4e7def2f6628c8fc6a3ebd88a00124d0cba6
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2023-10-07 11:36:19 -0500

    Programed the First Autonomous Parts
    We got the robot to go forward without using a controller. We didn't get it to stop yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -2,24 +2,37 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.Timer;
 
 @Autonomous
 public class BasicAuto extends OpMode {
     private RobotHardware robotHardware;
+    private ElapsedTime runTime;
     private MotorControl motorControl;
     //define variables
     double maxPower = 0.9;
     double denominator = 0;
     //Joystick movement
-    double axial   = 0;
-    double lateral =  0;
-    double yaw     =  0;
+    double axial = 1;
+    double lateral = 0;
+    double yaw = 0;
 
 
     @Override
     public void init() {
+        runTime = new ElapsedTime();
         robotHardware = new RobotHardware(this);
         robotHardware.init();
+        motorControl = new MotorControl(robotHardware);
+        telemetry.addData("init", "");
+        telemetry.update();
+        robotHardware.Frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robotHardware.Frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robotHardware.Backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robotHardware.Backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -29,13 +42,17 @@ public class BasicAuto extends OpMode {
 
     @Override
     public void start() {
-        motorControl.drive( axial, lateral, yaw, maxPower, denominator);
-
+        telemetry.addData("start", "");
+        telemetry.update();
+        runTime.reset();
     }
 
     @Override
     public void loop() {
-
+        telemetry.addData("runTime", runTime.seconds());
+        telemetry.update();
+        motorControl.drive(axial, lateral, yaw, maxPower, denominator);
+//        motorControl.drive(0.1, 0.2, 0.3, -0.9, 0.5);
     }
 }
 
