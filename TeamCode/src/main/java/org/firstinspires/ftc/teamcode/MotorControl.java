21ebe71bfcc7001b6d93e7fb4ef7633f514b8f7c
rhsftc <ronsearle@hotmail.com>
2023-10-08 11:53:06 -0500

    Refactored MotorControl and added FtcLib GamepadEx to CenterstageDrive teleop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorControl.java
@@ -1,26 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-import org.firstinspires.ftc.robotcore.external.JavaUtil;
-
+/*
+This class manages the actions of hardware defined in RobotHardware.
+Motor control should go here.
+ */
 public class MotorControl {
     private RobotHardware robotHardware;
+
     public MotorControl(RobotHardware robotHardware) {
         this.robotHardware = robotHardware;
     }
-    public void drive (double axial, double lateral, double yaw, double maxPower,
-        double denominator) {
 
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            double leftFrontPower = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower = axial - lateral + yaw;
-            double rightBackPower = axial + lateral - yaw;
-//            If the code below does not work, comment it out. Uncomment out the code below this code below.
-            denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(axial), Math.abs(lateral), Math.abs(yaw))), 1));
-            robotHardware.Frontleft.setPower(((axial + lateral + yaw) / denominator) * maxPower);
-            robotHardware.Backleft.setPower((((axial - lateral) + yaw) / denominator) * maxPower);
-            robotHardware.Frontright.setPower((((axial - lateral) - yaw) / denominator) * maxPower);
-            robotHardware.Backright.setPower((((axial + lateral) - yaw) / denominator) * maxPower);
-        }
+    public void drive(double axial, double lateral, double yaw, double maxPower) {
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(axial) + Math.abs(lateral) + Math.abs(axial * lateral), 1);
+        robotHardware.Frontleft.setPower(((axial + lateral + yaw) / denominator) * maxPower);
+        robotHardware.Backleft.setPower((((axial - lateral) + yaw) / denominator) * maxPower);
+        robotHardware.Frontright.setPower((((axial - lateral) - yaw) / denominator) * maxPower);
+        robotHardware.Backright.setPower((((axial + lateral) - yaw) / denominator) * maxPower);
+    }
 }
 
