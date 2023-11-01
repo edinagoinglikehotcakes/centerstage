e81db370d165d041b73b1304f04683379ff957d6
rhsftc <ronsearle@hotmail.com>
2023-11-01 14:27:39 -0500

    Building autonomous paths using camera

--- TeamCode/src/main/java/hotcakes/BasicAuto.java
+++ TeamCode/src/main/java/hotcakes/BasicAuto.java
@@ -3,19 +3,17 @@ package hotcakes;
 import android.util.Size;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.processors.ImageProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
 
-import java.util.Timer;
+import hotcakes.processors.ImageProcessor;
 
 @Autonomous
 public class BasicAuto extends OpMode {
@@ -25,10 +23,21 @@ public class BasicAuto extends OpMode {
     private VisionPortal visionPortal;
     private ImageProcessor.Selected selectedSpike;
     private ElapsedTime runTime;
+    private AutoState currentAutoState= AutoState.Idle;
+
+    public enum AutoState {
+        Idle,
+        PlacingPixelOnSpike,
+        PlacingPixelInBackstage,
+        PlacingPixelOnBackDrop,
+        ParkingInBackstage
+    }
 
     @Override
     public void init() {
-        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        //TODO Start left, need menu to offer both start positions
+        drive = new MecanumDrive(hardwareMap, new Pose2d(-30, -60, Math.toRadians(90)));
+
         imageProcessor = new ImageProcessor(telemetry);
         visionPortalBuilder = new VisionPortal.Builder();
         visionPortal = visionPortalBuilder.enableLiveView(true).
@@ -44,23 +53,33 @@ public class BasicAuto extends OpMode {
 
     @Override
     public void init_loop() {
-
+        selectedSpike = imageProcessor.getSelection();
+        telemetry.addData("Spike Identified", selectedSpike);
+        telemetry.update();
     }
 
     @Override
     public void start() {
-        selectedSpike = imageProcessor.getSelection();
-        telemetry.addData("Spike Identified", selectedSpike);
-        telemetry.update();
+        // Save resources
+        visionPortal.stopStreaming();
         runTime.reset();
     }
 
     @Override
     public void loop() {
-        TrajectoryActionBuilder ab = drive.actionBuilder(new Pose2d(new Vector2d(0, 0), Math.toRadians(0)));
         switch (selectedSpike) {
             case LEFT:
             case MIDDLE:
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose)
+                                .lineToX(30)
+                                .lineToX(-5)
+                                .turn(Math.toRadians(-90))
+                                .lineToX(80)
+                                .lineToX(-5)
+                                .splineTo(new Vector2d(60, -60), Math.toRadians(90))
+                                .build());
+                break;
             case RIGHT:
             case NONE:
             default:
