4355b78186ce35528611d536d2821b1edee077a4
rhsftc <ronsearle@hotmail.com>
2023-11-07 10:57:48 -0600

    Wait for camera to open. Display saturation levels.

--- TeamCode/src/main/java/hotcakes/processors/AutoTestEOCV.java
+++ TeamCode/src/main/java/hotcakes/processors/AutoTestEOCV.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.WhiteBalanceControl;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 
@@ -18,6 +19,7 @@ public class AutoTestEOCV extends OpMode {
     private VisionPortal.Builder visionPortalBuilder;
     private VisionPortal visionPortal;
     private ImageProcessor.Selected selectedSpike;
+    private WhiteBalanceControl whiteBalanceControl;
     private MecanumDrive mecanumDrive;
 
     @Override
@@ -34,21 +36,34 @@ public class AutoTestEOCV extends OpMode {
 
     @Override
     public void init_loop() {
+        // Wait for the camera to be open
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            return;
+        } else {
+            whiteBalanceControl = visionPortal.getCameraControl(WhiteBalanceControl.class);
+        }
+
+        if (whiteBalanceControl.getMode() != WhiteBalanceControl.Mode.AUTO) {
+            whiteBalanceControl.setMode(WhiteBalanceControl.Mode.AUTO);
+        }
+
         telemetry.addData("Init Identified", imageProcessor.getSelection());
-        telemetry.update();
+        telemetry.addData("Left", "%5.2f", imageProcessor.satRectLeft);
+        telemetry.addData("Middle", "%5.2f", imageProcessor.satRectMiddle);
+        telemetry.addData("Right", "%5.2f", imageProcessor.satRectRight);
     }
 
     @Override
     public void start() {
-            Actions.runBlocking(
-                    mecanumDrive.actionBuilder(mecanumDrive.pose)
+        Actions.runBlocking(
+                mecanumDrive.actionBuilder(mecanumDrive.pose)
 //                        .splineTo(new Vector2d(30,30),Math.PI /2)
 //                        .splineTo(new Vector2d(60,0),Math.PI)
-                            .turn(Math.toRadians(90))
-                            .build());
+                        .turn(Math.toRadians(90))
+                        .build());
         selectedSpike = imageProcessor.getSelection();
         telemetry.addData("Start Identified", selectedSpike);
-        telemetry.update();
         // Save resources
 //        visionPortal.setProcessorEnabled(imageProcessor, false);
 //        visionPortal.stopStreaming();
@@ -58,6 +73,5 @@ public class AutoTestEOCV extends OpMode {
     public void loop() {
         // Do your paths here.
         telemetry.addData("Identified", imageProcessor.getSelection());
-        telemetry.update();
     }
 }
